91d21169c2382f1b8601219ce2c6bfdf2b66b951
immadisairaj <immadirajendra.sai@gmail.com>
2022-09-15 12:40:45 +0530

    update base href

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "931a7f15fe19a78dd8b3ab76c1e8f2c7",
-"index.html": "9fc6ae69a9c47c00ad406ecc4402728c",
-"/": "9fc6ae69a9c47c00ad406ecc4402728c",
+"index.html": "2ee678fd83034954dd9a3f0da48f2f92",
+"/": "2ee678fd83034954dd9a3f0da48f2f92",
 "main.dart.js": "e49307fc6fe3a118d6c4a8286c481c47",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
